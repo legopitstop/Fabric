43fc6beda2bd6688a2fdf73200296c8ade16dd95
Legopitstop <1589lego@gmail.com>
2023-08-31 16:50:16 -0500

    1.20.1 - v1.0.2

--- More_Honey/CHANGELOG.md
+++ More_Honey/CHANGELOG.md
@@ -4,9 +4,15 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.0.1] - unreleased
+## [1.0.2] - 8-31-2023
+### General
+- Added issues URL.
+
+## [1.0.1] - 8-9-2023
 ### General
 - Some foods can be composted.
+- Requires Lot's More Food v0.0.2 or higher.
+- More Honey will now show up as a child mod under Lot's More Food when using modmenu.
 
 ## [1.0.0] - 7-31-23
 
--- More_Honey/src/main/java/com/legopitstop/morehoney/registry/MoreHoneyItems.java
+++ More_Honey/src/main/java/com/legopitstop/morehoney/registry/MoreHoneyItems.java
@@ -1,6 +1,9 @@
 package com.legopitstop.morehoney.registry;
 
+import com.legopitstop.morefood.item.MoreFoodComponents;
+import com.legopitstop.morefood.registry.MoreFoodItems;
 import com.legopitstop.morehoney.MoreHoney;
+import com.legopitstop.morehoney.item.HoneyLemonTeaItem;
 import com.legopitstop.morehoney.item.MoreHoneyFoodComponents;
 import com.legopitstop.morehoney.item.MoreHoneyItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -11,16 +14,16 @@ import net.minecraft.util.Identifier;
 
 public class MoreHoneyItems {
     // Create Items
-    public static final Item HONEY_LEMON_TEA = new MoreHoneyItem(new FabricItemSettings().maxCount(16).food(MoreHoneyFoodComponents.HONEY_LEMON_TEA));
+    public static final Item HONEY_LEMON_TEA = new HoneyLemonTeaItem(new FabricItemSettings().maxCount(16).food(MoreFoodComponents.HOT_BEVERAGE).recipeRemainder(MoreFoodItems.HOT_BEVERAGE_CUP));
     public static final Item HONEY_COOKED_BEEF = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_COOKED_BEEF));
     public static final Item HONEY_COOKED_CHICKEN = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_COOKED_CHICKEN));
     public static final Item HONEY_COOKED_MUTTON = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_COOKED_MUTTON));
     public static final Item HONEY_COOKED_PORKCHOP = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_COOKED_PORKCHOP));
     public static final Item HONEY_COOKIE = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_COOKIE));
-    public static final Item HONEY_PANCAKES = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_PANCAKES));
+    public static final Item HONEY_PANCAKES = new MoreHoneyItem(new FabricItemSettings().food(MoreFoodComponents.PANCAKES));
     public static final Item HONEY_SANDWICH = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_SANDWICH));
-    public static final Item HONEY_TOASTIE = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_TOASTIE));
-    public static final Item HONEY_WAFFLE = new MoreHoneyItem(new FabricItemSettings().food(MoreHoneyFoodComponents.HONEY_WAFFLE));
+    public static final Item HONEY_TOASTIE = new MoreHoneyItem(new FabricItemSettings().food(MoreFoodComponents.CHEESE_TOASTIE));
+    public static final Item HONEY_WAFFLE = new MoreHoneyItem(new FabricItemSettings().food(MoreFoodComponents.WAFFLE));
 
     public static void register() {
         // Register Items
