e71285e47c1864e77ae5f548501be989b6521757
Legopitstop <1589lego@gmail.com>
2023-10-04 20:13:39 -0500

    Added simple magnets

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Fabric Mods
 
 ## Table of Contents
+- [Armor Stand Poses](./Armorstand_Poses/README.md)
+- [Basalt Blocks](./Basalt_Blocks/README.md)
 - [Bright](./Bright/README.md)
+- [Canned](./Canned/README.md)
+- [Lightning Bolt Glass](./Lightning_Bolt_Glass/README.md)
+- [More Block](./More_Block/README.md)
 - [More Gold](./More_Gold/README.md)
 - [More Honey](./More_Honey/README.md)
-- [More Block](./More_Block/README.md)
-- [Lightning Bolt Glass](./Lightning_Bolt_Glass/README.md)
-- [Canned](./Canned/README.md)
 - [More Pumpkin](./More_Pumpkin/README.md)
-- [Basalt Blocks](./Basalt_Blocks/README.md)
-- [Armor Stand Poses](./Armorstand_Poses/README.md)
+- [Simple Magnets](./Simple_Magnets/README.md)
 
 - Planned:
     - [Construction Blocks](./Construction_Blocks/README.md)
     - [More Blocks](./More_Blocks/README.md)
-    - [Simple Magnets](./Simple_Magnets/README.md)
     - [Spawner Craft](./Spawner_Craft/README.md)
     - [Ultimate Block Breaker](./Ultimate_Block_Breaker/README.md)
---
+++ Simple_Magnets/src/main/java/com/legopitstop/magnet/item/MagnetItem.java
@@ -0,0 +1,38 @@
+package com.legopitstop.magnet.item;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class MagnetItem extends ToolItem implements Vanishable {
+    public double radius;
+    public int delay;
+
+    public MagnetItem(ToolMaterial toolMaterial,double radius, int delay, Settings settings) {
+        super(toolMaterial, settings);
+        this.radius = radius;
+        this.delay = delay;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.getItemCooldownManager().set(this, this.delay);
+        ItemStack mainHand = user.getStackInHand(hand);
+        if (world.isClient) {return TypedActionResult.pass(mainHand);}
+        mainHand.damage(1, user, (player) -> user.sendToolBreakStatus(user.getActiveHand()));
+        List<ItemEntity> items = world.getEntitiesByType(EntityType.ITEM, Box.of(user.getPos(), this.radius*2, this.radius*2, this.radius*2), (e) -> {
+            return true;
+        });
+        for (ItemEntity item : items) {
+            item.setPosition(user.getPos());
+        }
+        return TypedActionResult.success(mainHand);
+    }
+}
